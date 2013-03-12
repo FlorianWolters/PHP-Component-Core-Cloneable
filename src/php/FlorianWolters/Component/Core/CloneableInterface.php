9766f9f8cee5e957668cefb7ea66ab0ef1e1448a
Florian Wolters <wolters.fl@gmail.com>
2013-03-12 19:14:40 +0100

    Enhanced documentation. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/php/FlorianWolters/Component/Core/CloneableInterface.php
+++ src/php/FlorianWolters/Component/Core/CloneableInterface.php
@@ -2,25 +2,29 @@
 namespace FlorianWolters\Component\Core;
 
 /**
- * A class implements the {@see CloneableInterface} to indicate to the
+ * A class implements the {@see CloneableInterface} to indicate to the magic
  * `__clone` method of a class that it is legal for that method to make a
  * field-for-field copy of instances of that class.
+ *
+ * By convention, classes that implement this interface should use either the
+ * {@see ShallowCloneTrait} or the {@see DeepCloneTrait}.
+ *
+ * Invoking the magic `__clone` method on an instance that does not implement
+ * the {@see CloneableInterface} results in the exception {@see
+ * CloneNotSupportedException} being thrown when using the {@see
+ * ShallowCloneTrait} or the {@see DeepCloneTrait}.
+ *
+ * Note that this interface does not contain the magic `__clone` method.
+ * Therefore, it is not possible to clone an object merely by virtue of the fact
+ * that it implements this interface.
  *
  * @author    Florian Wolters <wolters.fl@gmail.com>
  * @copyright 2011-2013 Florian Wolters
  * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
  * @link      http://github.com/FlorianWolters/PHP-Component-Core-Cloneable
- * @see       CloneNotSupportedException, CloneNotSupportedTrait
+ * @see       CloneNotSupportedException
  * @since     Interface available since Release 0.1.0
  */
 interface CloneableInterface
 {
-    /**
-     * Creates and returns a copy of the object.
-     *
-     * The precise meaning of "copy" may depend on the class of the object.
-     *
-     * @return object The copy of the object.
-     */
-    public function __clone();
 }
