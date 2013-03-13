8daaf325edcd1e85fc7970cae3a20ecb6fe8489e
Florian Wolters <wolters.fl@gmail.com>
2013-03-13 15:39:24 +0100

    Added static utility class. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

---
+++ src/php/FlorianWolters/Component/Core/CloneUtils.php
@@ -0,0 +1,113 @@
+<?php
+namespace FlorianWolters\Component\Core;
+
+use \InvalidArgumentException;
+
+/**
+ * The static class {@see CloneUtils} offers operations to clone objects.
+ *
+ * @author    Florian Wolters <wolters.fl@gmail.com>
+ * @copyright 2011-2013 Florian Wolters
+ * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
+ * @link      http://github.com/FlorianWolters/PHP-Component-Core-Cloneable
+ * @see       CloneNotSupportedException
+ * @since     Class available since Release 0.2.0
+ */
+final class CloneUtils
+{
+    // @codeCoverageIgnoreStart
+
+    /**
+     * {@see CloneUtils} instances can **NOT** be constructed in standard
+     * programming.
+     *
+     * Instead, the class should be used as:
+     * /---code php
+     * $clone = CloneUtils::copyDeep($original);
+     * \---
+     */
+    protected function __construct()
+    {
+    }
+
+    // @codeCoverageIgnoreEnd
+
+    /**
+     * Creates and returns a deep copy of the specified object.
+     *
+     * The deep copy is **recursive**, so it cannot be used if the specified
+     * object has one or more circular or parental references.
+     *
+     * @param object $source The object to copy.
+     *
+     * @return object The deep copy of the object.
+     * @throws InvalidArgumentException If `$source` is not an object.
+     */
+    public static function copyDeep($source)
+    {
+        if (false === \is_object($source)) {
+            throw new InvalidArgumentException('$source is not an object.');
+        }
+
+        return \unserialize(\serialize($source));
+    }
+
+    /**
+     * Creates and returns a copy of the specified object.
+     *
+     * The precise meaning of "copy" may depend on the class of the object.
+     *
+     * @param CloneableInterface $source The object to clone.
+     *
+     * @return CloneableInterface The cloned object.
+     */
+    public static function copy(CloneableInterface $source)
+    {
+        return clone $source;
+    }
+
+    /**
+     * Throws a {@see CloneNotSupportedException} if the specified object does
+     * not implement {@see CloneableInterface}.
+     *
+     * @param CloneableInterface $obj The object to check.
+     *
+     * @return void
+     * @throws CloneNotSupportedException If the object `$obj` does not
+     *                                    implement {@see CloneableInterface}.
+     */
+    public static function throwCloneNotSupportedExceptionIfNotInstanceOfCloneableInterface($obj)
+    {
+        if (false === self::isInstanceOfCloneableInterface($obj)) {
+            self::throwCloneNotSupportedException();
+        }
+    }
+
+    /**
+     * Checks whether the specified object is an instance of {@see
+     * CloneableInterface}.
+     *
+     * @param CloneableInterface $obj The object to check.
+     *
+     * @return boolean `true` if the object `$obj` is an instance of {@see
+     *                 CloneableInterface}; `false` otherwise.
+     */
+    private static function isInstanceOfCloneableInterface($obj)
+    {
+        return (true === ($obj instanceof CloneableInterface));
+    }
+
+    /**
+     * Throws a {@see CloneNotSupportedException}.
+     *
+     * @return void
+     * @throws CloneNotSupportedException Always.
+     */
+    private static function throwCloneNotSupportedException()
+    {
+        throw new CloneNotSupportedException(
+            'The object does not implement ' . __NAMESPACE__
+            . '\CloneableInterface.'
+        );
+    }
+}
---
+++ src/tests/unit-tests/php/FlorianWolters/Component/Core/CloneUtilsTest.php
@@ -0,0 +1,133 @@
+<?php
+namespace FlorianWolters\Component\Core;
+
+use \stdClass;
+
+/**
+ * Test class for {@see CloneUtils}.
+ *
+ * @author    Florian Wolters <wolters.fl@gmail.com>
+ * @copyright 2011-2013 Florian Wolters
+ * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
+ * @link      http://github.com/FlorianWolters/PHP-Component-Core-Cloneable
+ * @since     Class available since Release 0.1.0
+ *
+ * @covers    FlorianWolters\Component\Core\CloneUtils
+ */
+class CloneUtilsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var stdClass
+     */
+    private $original;
+
+    /**
+     * Sets up the fixture.
+     *
+     * This method is called before a test is executed.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $secondReference = new stdClass;
+        $secondReference->scalar = 'second reference';
+
+        $firstReference = new stdClass;
+        $firstReference->scalar = 'first reference';
+        $firstReference->object = $secondReference;
+
+        $this->original = new stdClass;
+        $this->original->scalar = 'object';
+        $this->original->scalarArray = ['foo', 'bar'];
+        $this->original->object = $firstReference;
+        $this->original->objectArray = [new stdClass, &$this->original];
+        $this->original->selfReference = &$this->original;
+    }
+
+    /**
+     * @return void
+     *
+     * @coversDefaultClass copyDeep
+     * @test
+     */
+    public function testCopyDeepCanHandleScalars()
+    {
+        $actual = CloneUtils::copyDeep($this->original);
+
+        $this->original->scalar = '';
+        $this->original->scalarArray = [];
+
+        $this->assertEquals('object', $actual->scalar);
+        $this->assertEquals(['foo', 'bar'], $actual->scalarArray);
+    }
+
+    /**
+     * @return void
+     *
+     * @coversDefaultClass copyDeep
+     * @test
+     */
+    public function testCopyDeepCanHandleReferences()
+    {
+        $actual = CloneUtils::copyDeep($this->original);
+
+        $this->original->object->scalar = '';
+        $this->original->objectArray = [];
+
+        $this->assertEquals('first reference', $actual->object->scalar);
+        $this->assertNotEquals(
+            $this->original->objectArray,
+            $actual->objectArray
+        );
+    }
+
+    /**
+     * @return void
+     *
+     * @coversDefaultClass copyDeep
+     * @test
+     */
+    public function testCopyDeepCanHandleRecursiveReferences()
+    {
+        $actual = CloneUtils::copyDeep($this->original);
+
+        $this->original->object->object->scalar = '';
+
+        $this->assertEquals(
+            'second reference',
+            $actual->object->object->scalar
+        );
+    }
+
+    /**
+     * @return void
+     *
+     * @coversDefaultClass copyDeep
+     * @test
+     */
+    public function testCopyDeepCanHandleSelfReferences()
+    {
+        $actual = CloneUtils::copyDeep($this->original);
+
+        $this->assertTrue($actual === $actual->selfReference);
+        $this->assertFalse($this->original === $actual->selfReference);
+
+        $this->assertTrue($actual === $actual->objectArray[1]);
+        $this->assertFalse(
+            $this->original->objectArray[1] === $actual->objectArray[1]
+        );
+    }
+
+    /**
+     * @return void
+     *
+     * @coversDefaultClass copyDeep
+     * @expectedException \InvalidArgumentException
+     * @test
+     */
+    public function testCopyDeepThrowsExceptionIfNoObject()
+    {
+        CloneUtils::copyDeep(null);
+    }
+}
