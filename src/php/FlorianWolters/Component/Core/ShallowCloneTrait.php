0679e0751a05fb1a6892525c9d2bf690393b3ff1
Florian Wolters <wolters.fl@gmail.com>
2013-03-13 15:41:09 +0100

    - Refactored ("move method") exception handling. - Added modifier "final" to the method "__clone". Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/php/FlorianWolters/Component/Core/ShallowCloneTrait.php
+++ src/php/FlorianWolters/Component/Core/ShallowCloneTrait.php
@@ -2,7 +2,7 @@
 namespace FlorianWolters\Component\Core;
 
 /**
- * A class uses the {@see ShallowCloneTrait} to indicate to the `__clone`
+ * A class uses the {@see ShallowCloneTrait} to indicate to the magic `__clone`
  * method of a class that it is **legal** for that method to make a **shallow*
  * field-for-field copy of instances of that class.
  *
@@ -15,39 +15,18 @@ namespace FlorianWolters\Component\Core;
 trait ShallowCloneTrait
 {
     /**
-     * Creates a copy of the object.
+     * Creates a copy of this object.
      *
-     * "copy" means performing a shallow copy of all of the properties of the
-     * object.
+     * "copy" means performing a **shallow** copy of all of the properties of
+     * this object.
      *
      * @return void
      * @throws CloneNotSupportedException If the object using this trait does
      *                                    not implement {@see
      *                                    CloneableInterface}.
      */
-    public function __clone()
+    final public function __clone()
     {
-        $this->throwCloneNotSupportedExceptionIfNotImplementsCloneableInterface($this);
-    }
-
-    /**
-     * Throws a {@see CloneNotSupportedException} if the specified object does
-     * not implement {@see CloneableInterface}.
-     *
-     * @param CloneableInterface $obj The object to check.
-     *
-     * @return void
-     * @throws CloneNotSupportedException If `$obj` does not implement {@see
-     *                                    CloneableInterface}.
-     * @todo Find a better name for the method.
-     */
-    private function throwCloneNotSupportedExceptionIfNotImplementsCloneableInterface($obj)
-    {
-        if (false === $obj instanceof CloneableInterface) {
-            throw new CloneNotSupportedException(
-                'An object must implement ' . __NAMESPACE__
-                . '\CloneableInterface to be clonable.'
-            );
-        }
+        CloneUtils::throwCloneNotSupportedExceptionIfNotInstanceOfCloneableInterface($this);
     }
 }
