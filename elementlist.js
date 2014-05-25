68a22bb1e25d5933973369d4e795226eaa58fa30
Florian Wolters <wolters.fl@gmail.com>
2014-05-25 22:22:23 +0200

    Created latest API documentation with phpDocumentor (which is still buggy).
    Signed-off-by: Florian Wolters <wolters.fl@gmail.com>

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Core\\CloneableInterface"],["c","FlorianWolters\\Component\\Core\\CloneNotSupportedException"],["c","FlorianWolters\\Component\\Core\\CloneNotSupportedTrait"]];
